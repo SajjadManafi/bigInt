3576836a27227038fd48f254c35f676417458d19
SajjdManafi <sajadmanafi8@gmail.com>
2021-07-05 19:02:44 +0430

    static func for create class from String

--- src/bignumber/bigInt.java
+++ src/bignumber/bigInt.java
@@ -23,4 +23,34 @@ public class bigInt {
 
 
 
+    public static bigInt fromString(String s){
+
+        boolean hasSign = false;
+        sign sign = bignumber.sign.positive;
+        int StrLen = s.length();
+        int remove = 1;
+
+        if (s.charAt(0) == '+' || s.charAt(0) == '-'){
+            hasSign = true;
+            remove = 0;
+            StrLen -= 1;
+
+            if (s.charAt(0) == '-')
+                sign = bignumber.sign.negative;
+        }
+        byte[] digits = new byte[StrLen];
+        int j = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (hasSign && i == 0) continue;
+            if (!Character.isDigit(s.charAt(i))) throw new IllegalArgumentException("Bad Number: " + s);
+            digits[j++] = (byte) (s.charAt(s.length() -i -remove) - '0');
+        }
+        return new bigInt(digits , sign);
+    }
+
+
+
+
+
 }
