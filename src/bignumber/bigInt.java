a978c232f0ee8a08d609f1177a731e7fcdfd7986
SajjdManafi <sajadmanafi8@gmail.com>
2021-07-06 09:55:55 +0430

    add getTempNumberIndex fucn and 0.5 of suntract func

--- src/bignumber/bigInt.java
+++ src/bignumber/bigInt.java
@@ -25,19 +25,20 @@ public class bigInt {
     }
 
     // Addition of two numbers
-    bigInt add(bigInt a){
+    bigInt add(bigInt a) {
         int temp = 0;
-        int maxLen = bigInt.max(a , this).digits.length;
-        byte[] sumDigits = new byte[maxLen+1];
-        int one , two , sum = 0 , j = 0;
+        int maxLen = bigInt.max(a, this).digits.length;
+        byte[] sumDigits = new byte[maxLen + 1];
+        int one, two, sum = 0, j = 0;
 
         for (int i = 0; i < maxLen; i++) {
-            one = 0; two = 0;
+            one = 0;
+            two = 0;
             if (i < a.digits.length) one = a.digits[i];
             if (i < digits.length) two = digits[i];
             sum = one + two + temp;
             temp = 0;
-            if (sum >= 10){
+            if (sum >= 10) {
                 temp = 1;
                 sum -= 10;
             }
@@ -47,33 +48,42 @@ public class bigInt {
             if (j == sumDigits.length - 1 && temp == 1) sumDigits[j] = (byte) 1;
 
         }
-        return new bigInt(sumDigits , sign);
+        return new bigInt(sumDigits, sign);
     }
 
-    bigInt subtract(bigInt a){
-        bigInt[] sortedBigInts = bigInt.sort(a , this);
+    bigInt subtract(bigInt a) {
+        bigInt[] sortedBigInts = bigInt.sort(a, this);
         bigInt larger = sortedBigInts[0], smaller = sortedBigInts[1];
+        byte[] resDigits = new byte[larger.digits.length];
+        int one, two, sum = 0, j = 0;
+        for (int i  = 0; i < larger.digits.length; i++) {
+            one = larger.digits[i];
+            if (i < smaller.digits.length) two = smaller.digits[i];
+
+
+        }
+
 
         return bigInt.fromString("+1");
     }
 
-    bigInt multyply(bigInt a){
+    bigInt multyply(bigInt a) {
         return bigInt.fromString("+1");
     }
 
-    bigInt divide(bigInt a){
+    bigInt divide(bigInt a) {
         return bigInt.fromString("+1");
     }
 
     // create bigInt from String
-    public static bigInt fromString(String s){
+    public static bigInt fromString(String s) {
 
         boolean hasSign = false;
         sign sign = bignumber.sign.positive;
         int StrLen = s.length();
         int remove = 1;
 
-        if (s.charAt(0) == '+' || s.charAt(0) == '-'){
+        if (s.charAt(0) == '+' || s.charAt(0) == '-') {
             hasSign = true;
             remove = 0;
             StrLen -= 1;
@@ -87,9 +97,9 @@ public class bigInt {
         for (int i = 0; i < s.length(); i++) {
             if (hasSign && i == 0) continue;
             if (!Character.isDigit(s.charAt(i))) throw new IllegalArgumentException("Bad Number: " + s);
-            digits[j++] = (byte) (s.charAt(s.length() -i -remove) - '0');
+            digits[j++] = (byte) (s.charAt(s.length() - i - remove) - '0');
         }
-        return new bigInt(digits , sign);
+        return new bigInt(digits, sign);
     }
 
     // to String
@@ -100,41 +110,40 @@ public class bigInt {
         if (sign == bignumber.sign.positive) str.append("+");
         else str.append("-");
         // append digits
-        for (int i = digits.length - 1; i >= 0 ; i--) {
+        for (int i = digits.length - 1; i >= 0; i--) {
             str.append(digits[i]);
         }
         return str.toString();
     }
 
     // getting a larger number between two numbers
-    private static bigInt max(bigInt a , bigInt b){
+    private static bigInt max(bigInt a, bigInt b) {
         if (a.digits.length >= b.digits.length) return a;
         else return b;
     }
+
     // getting a smaller number between two numbers
-    private static bigInt min(bigInt a , bigInt b){
+    private static bigInt min(bigInt a, bigInt b) {
         if (a.digits.length <= b.digits.length) return a;
         else return b;
     }
 
-    private static bigInt[] sort(bigInt a, bigInt b){
+    // sort larger to smaller
+    private static bigInt[] sort(bigInt a, bigInt b) {
         bigInt[] sortedBigInts = new bigInt[2];
         sortedBigInts[0] = a;
         sortedBigInts[1] = b;
         if (a.digits.length > b.digits.length) {
-         // nothing
-        }
-        else if (b.digits.length > a.digits.length){
+            // nothing
+        } else if (b.digits.length > a.digits.length) {
             sortedBigInts[0] = b;
             sortedBigInts[1] = a;
-        }
-        else{
+        } else {
 
             for (int i = 0; i < a.digits.length; i++) {
-                if ((int) a.digits[i] > (int) b.digits[i]){
+                if ((int) a.digits[i] > (int) b.digits[i]) {
                     break;
-                }
-                else if ((int) a.digits[i] < (int) b.digits[i]){
+                } else if ((int) a.digits[i] < (int) b.digits[i]) {
                     sortedBigInts[0] = b;
                     sortedBigInts[1] = a;
                     break;
@@ -144,12 +153,20 @@ public class bigInt {
         return sortedBigInts;
     }
 
+    private int getTempNumberIndex(bigInt number , int currentState , int toSubtract){
+        for (int i = currentState + 1; i < number.digits.length; i++) {
+            if ((int) number.digits[i] - toSubtract >= 0) return i;
+        }
+        return -1;
+    }
+
 
     public static void main(String[] args) {
-        bigInt a = bigInt.fromString("238432847237422862525452454257498545845845237432438264329843");
+        bigInt a = bigInt.fromString("5848574752764137329329319585465645374314813493818197547245531");
         bigInt b = bigInt.fromString("5848574752764137329329319585465645374314813493818197547245532");
 
         bigInt sum = a.add(b);
+        bigInt alo = a.subtract(b);
         System.out.println(sum);
     }
 
