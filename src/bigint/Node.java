e7744815e80357de5935dba1f602ec5e056d45f8
SajjdManafi <sajadmanafi8@gmail.com>
2021-07-08 22:43:39 +0430

    add setter and getter funcs

--- src/bigint/Node.java
+++ src/bigint/Node.java
@@ -2,13 +2,45 @@ package bigint;
 import java.util.ArrayList;
 
 public class Node {
-    private String contents;
-    private BigInt bigInt;
-    private Node left;
-    private Node right;
+     private String contents;
+     private BigInt bigInt;
+     private Node left;
+     private Node right;
 
-    private boolean isLeaf(){
+    boolean isLeaf(){
         return (left == null && right == null);
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public BigInt getBigInt() {
+        return bigInt;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public void setBigInt(BigInt bigInt) {
+        this.bigInt = bigInt;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
     }
 
     static Node readTree(ArrayList<String> strArray) {
